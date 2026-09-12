9cf02b94230b68239b0f39fa1a58d9ed9425a05d
Jmeoyales <Jmeoyales@brc.pshs.edu,ph>
2026-09-12 22:55:04 +0800

    revert fully

--- q1/OOP Act II/classImplementation.py
+++ q1/OOP Act II/classImplementation.py
@@ -1,75 +1,128 @@
-
-
 class Digitalsafe:
-    def __init__(self, safename: str = "", maxstorage: int = 0, username: str = "", passkey: str = ""):
-    # public
+    def __init__(self, safename: str = "", maxstorage: int = 0, username: str = "", passkey: str = "", compartments: int = 1):
+        # Public
         self.Safename: str = safename
         self.maxstorage: int = maxstorage
-    
-    # private
+        # Private
         self.__Username: str = username
         self.__User_passkey: str = passkey
         self.__Available: bool = False
-        self.__Compartments: int = 1
-        self.__files: str = ""
+        self.__Compartments: int = compartments
+        self.__files: dict[int, list[str]] = {i: [] for i in range(1, self.__Compartments + 1)}
 
+    def total_file_count(self) -> int:
+        return sum(len(files) for files in self.__files.values())
 
-#fucntion to open safe to user
-    def accesssafe(self, given_paskey: str,) -> bool:
+    def accesssafe(self, given_paskey: str) -> bool:
         if given_paskey == self.__User_passkey:
-            print("access granted")
+            print("Access granted")
             return True
         else:
-            print("access not granted")
+            print("Access denied")
             return False
+        
+    def addfile(self, filename: str, compartment: int = 1) -> None:
+        # Checks if compartment exists
+        if compartment not in self.__files:
+            print(f"Inputted compartment as destination {compartment} does not exist")
+            return
 
-    def addfile(self, filename = str) -> None:
-        #adds files
-        if self.__files == "":
-            self.__files = filename
-        else:
-            self.__files += f"{filename}"
-        #changes availablity of files to true meaning there are files
+        # Checks if input will exceed storage capacity
+        if self.total_file_count() >= self.maxstorage:
+            print(f"Cannot store file {filename}: exceeded storage capacity of {self.maxstorage}")
+            return
+
+        self.__files[compartment].append(filename)
         self.__Available = True
-        print("you have stored"+ filename)
+        print(f"Successfully added file to digital safe {self.Safename}")
 
-    def take_files(self, filename) -> str:
-        if not self.__Available:
+    def take_files(self, filename: str, compartment: int = 1) -> str:
+        if compartment in self.__files and filename in self.__files[compartment]:
+            self.__files[compartment].remove(filename)
+            if self.total_file_count() == 0:
+                self.__Available = False
+            return filename
+        else:
+            print("File was not found in destination")
             return ""
-        taken = self.__files
-        self.__files = ""
-        self.__Available = False  # Set back to False since storage is now empty
-        return taken
 
     def display_info(self) -> None:
-        files_present = "yes there are files present" if self.__Available else "no there are no files"
-        file_list = self.__files if self.__files else "none"
-        print(f"Safe name: {self.Safename} | Capacity:  {self.maxstorage} | ")
+        files_present = "yes" if self.__Available else "no"
+        print(f"\nSafe name: {self.Safename} | Total storage used: {self.total_file_count()}/{self.maxstorage} | Files available: {files_present}")
+        print(f"Owner: {self.__Username}")
+        print("Compartments:")
+        for comp_num, files_list in self.__files.items():
+            content = ", ".join(files_list) if files_list else "No files present"
+            print(f"  Compartment {comp_num}: {content}")
+
+    @staticmethod
+    def create_safe() -> "Digitalsafe":
+        print("\n Make a safe")
+        safename = input("Enter safename: ")
+        maxstorage = int(input("Enter max storage capacity: "))
+        username = input("Enter username: ")
+        passkey = input("Enter a passkey you will use: ")
+        compartments = int(input("Enter number of compartments: "))
+        
+        return Digitalsafe(safename, maxstorage, username, passkey, compartments)
+
+    @staticmethod
+    def initialize_safe_TEST():
+        # Object 1
+        safe1 = Digitalsafe(
+            safename="Personal Safe", 
+            maxstorage=5, 
+            username="van lester", 
+            passkey="vault12390", 
+            compartments=2
+        )
+        
+        # Object 2
+        safe2 = Digitalsafe(
+            safename="Corporate Vault", 
+            maxstorage=100, 
+            username="BIRON", 
+            passkey="larper", 
+            compartments=5
+        )
+        
+        print("\nInitial state of objects:")
+        safe1.display_info()
+        safe2.display_info()
+        
+        print("\nModification of object 1:")
+        safe1.addfile("passwords.txt", compartment=1)
+        safe1.addfile("tax_2026.pdf", compartment=2)
+        
+        print("\nAfter modification of object 1:")
+        safe1.display_info()
+        
+        print("\nProof of object 2's independence:")
+        safe2.display_info()
+        return safe1, safe2
+
+class Safemanger:
+    
 
 
+# Main
 if __name__ == "__main__":
-    safe1 = Digitalsafe("Personal Safe", 5, "van lester", "vault12390")
-    safe2 = Digitalsafe("Corporate Vault", 100, "BIRON", "admin99")
+    while True:
+        print("\n---------- Choices:")
+        print("1. Demonstrate class")
+        print("2. Create safe")
+        print("3. Exit")
+        
+        choice = input("Enter choice: ").strip()
 
-    print("=== BEFORE ACTION ===")
-    print("Object 1 Initial State:")
-    safe1.display_info()
-    print("\nObject 2 Initial State:")
-    safe2.display_info()
-
-    print("\n=== PERFORMING ACTIONS ON OBJECT 1 ONLY ===")
-    safe1.addfile("passwords.txt")
-    safe1.addfile("tax_2026.pdf")
-
-    print("\n=== AFTER ACTION ===")
-    print("Object 1 Updated State:")
-    safe1.display_info()
-    print("\nObject 2 Unchanged State:")
-    safe2.display_info()
-
-
-
-    
-    
-         
-
+        if choice == "1":
+            Digitalsafe.initialize_safe_TEST()
+        elif choice == "2":
+            new_safe = Digitalsafe.create_safe()
+            print("\nSafe successfully created!")
+            new_safe.display_info()
+        elif choice == "3":
+            print("Exiting program. Goodbye!")
+            break
+        else:
+            print("Invalid choice, please select 1, 2, or 3.")
