49444b31e636f8c12020c9dbdedfc7cfb7261945
Jmeoyales <Jmeoyales@brc.pshs.edu,ph>
2026-09-12 22:04:02 +0800

    Revert "overhaul still underway continuing to update original class to be able to accept other class"
    This reverts commit 282f1df6a4332f164e03163afb61499c150a81c9.

--- q1/OOP Act II/classImplementation.py
+++ q1/OOP Act II/classImplementation.py
@@ -52,82 +52,33 @@ class Digitalsafe:
             return ""
 
     def display_info(self) -> None:
-        files_present = "yes" if self.__Available else "no"
-        print(f"\nSafe name: {self.Safename} | Total storage used: {self.total_file_count()}/{self.maxstorage} | Files available: {files_present}")
-        print(f"Owner: {self.__Username}")
-        print("Compartments:")
-        for comp_num, files_list in self.__files.items():
-            content = ", ".join(files_list) if files_list else "No files present"
-            print(f"  Compartment {comp_num}: {content}")
-
-    @staticmethod
-    def create_safe() -> "Digitalsafe":
-        print("\n Make a safe")
-        safename = input("Enter safename: ")
-        maxstorage = int(input("Enter max storage capacity: "))
-        username = input("Enter username: ")
-        passkey = input("Enter a passkey you will use: ")
-        compartments = int(input("Enter number of compartments: "))
-        
-        return Digitalsafe(safename, maxstorage, username, passkey, compartments)
-
-    @staticmethod
-    def initialize_safe_TEST():
-        # Object 1
-        safe1 = Digitalsafe(
-            safename="Personal Safe", 
-            maxstorage=5, 
-            username="van lester", 
-            passkey="vault12390", 
-            compartments=2
-        )
-        
-        # Object 2
-        safe2 = Digitalsafe(
-            safename="Corporate Vault", 
-            maxstorage=100, 
-            username="BIRON", 
-            passkey="larper", 
-            compartments=5
-        )
-        
-        print("\nInitial state of objects:")
-        safe1.display_info()
-        safe2.display_info()
-        
-        print("\nModification of object 1:")
-        safe1.addfile("passwords.txt", compartment=1)
-        safe1.addfile("tax_2026.pdf", compartment=2)
-        
-        print("\nAfter modification of object 1:")
-        safe1.display_info()
-        
-        print("\nProof of object 2's independence:")
-        safe2.display_info()
-        return safe1, safe2
-
-class Safemanger:
-    
+        files_present = "yes" if self.__Available == "no"
+        files_list = "| ". join(self.__files) if self.__files 
 
 
-# Main
 if __name__ == "__main__":
-    while True:
-        print("\n---------- Choices:")
-        print("1. Demonstrate class")
-        print("2. Create safe")
-        print("3. Exit")
-        
-        choice = input("Enter choice: ").strip()
+    safe1 = Digitalsafe("Personal Safe", 5, "van lester", "vault12390")
+    safe2 = Digitalsafe("Corporate Vault", 100, "BIRON", "admin99")
 
-        if choice == "1":
-            Digitalsafe.initialize_safe_TEST()
-        elif choice == "2":
-            new_safe = Digitalsafe.create_safe()
-            print("\nSafe successfully created!")
-            new_safe.display_info()
-        elif choice == "3":
-            print("Exiting program. Goodbye!")
-            break
-        else:
-            print("Invalid choice, please select 1, 2, or 3.")
+    print("=== BEFORE ACTION ===")
+    print("Object 1 Initial State:")
+    safe1.display_info()
+    print("\nObject 2 Initial State:")
+    safe2.display_info()
+
+    print("\n=== PERFORMING ACTIONS ON OBJECT 1 ONLY ===")
+    safe1.addfile("passwords.txt")
+    safe1.addfile("tax_2026.pdf")
+
+    print("\n=== AFTER ACTION ===")
+    print("Object 1 Updated State:")
+    safe1.display_info()
+    print("\nObject 2 Unchanged State:")
+    safe2.display_info()
+
+
+
+    
+    
+         
+
